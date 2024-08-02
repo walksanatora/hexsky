6978bc31fde784473d7294b2541e284099bf0d2a
Walker Fowlkes <walkerffo22@gmail.com>
2024-08-02 10:05:06 -0700

    I wanna die... why does this not return the loaded version... I should probally check the other pattenrs

--- common/src/main/java/net/walksanator/hexxyskies/casting/patterns/OpPosToShip.kt
+++ common/src/main/java/net/walksanator/hexxyskies/casting/patterns/OpPosToShip.kt
@@ -6,9 +6,12 @@ import at.petrak.hexcasting.api.casting.getBlockPos
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.iota.NullIota
 import at.petrak.hexcasting.api.misc.MediaConstants
+import net.minecraft.server.level.ServerLevel
 import net.walksanator.hexxyskies.casting.iotas.ShipIota
 import net.walksanator.hexxyskies.ship.getShipDataHolder
+import org.valkyrienskies.core.api.ships.ServerShip
 import org.valkyrienskies.mod.common.getShipManagingPos
+import org.valkyrienskies.mod.common.shipObjectWorld
 
 object OpPosToShip : ConstMediaAction {
     override val argc: Int = 1
@@ -17,8 +20,13 @@ object OpPosToShip : ConstMediaAction {
         val pos = args.getBlockPos(0, argc)
         env.assertPosInRange(pos)
         val ship = env.world.getShipManagingPos(pos)?.let {
-            if (it.getShipDataHolder().cloaked) {null} else {ShipIota(it.id, it.slug)}
+            if (it.loaded(env.world).getShipDataHolder().cloaked) {null} else {ShipIota(it.id, it.slug)}
         }?: NullIota()
         return listOf(ship)
     }
+}
+
+fun ServerShip.loaded(world: ServerLevel): ServerShip {
+    val sow = world.shipObjectWorld
+    return sow.loadedShips.getById(this.id)?: sow.allShips.getById(this.id)!!
 }
