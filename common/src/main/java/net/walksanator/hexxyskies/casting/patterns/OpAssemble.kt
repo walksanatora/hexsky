9ce534f63ea995bc97fe17602873a68f18cfa78d
Walker Fowlkes <walkerffo22@gmail.com>
2024-08-03 18:01:07 -0700

    why the HELL is it offset by one block

--- common/src/main/java/net/walksanator/hexxyskies/casting/patterns/OpAssemble.kt
+++ common/src/main/java/net/walksanator/hexxyskies/casting/patterns/OpAssemble.kt
@@ -13,24 +13,27 @@ import net.walksanator.hexxyskies.ship.getShipDataHolder
 import org.valkyrienskies.core.util.datastructures.DenseBlockPosSet
 import org.valkyrienskies.mod.common.assembly.createNewShipWithBlocks
 import org.valkyrienskies.mod.common.util.toJOML
+import ram.talia.hexal.api.div
 import kotlin.math.ln
 
 object OpAssemble : VariableMediaAction {
     override val argc: Int = 1
     override fun render(args: List<Iota>, env: CastingEnvironment): VariableMediaAction.Result {
-        val blocks = args.getList(0,argc).filterIsInstance<Vec3Iota>().filter { env.isVecInAmbit(it.vec3) }.map {it.vec3}
-        return Spell(blocks)
+        val blocks = args.getList(0,argc)
+        val filter = blocks.filterIsInstance<Vec3Iota>().filter { env.isVecInAmbit(it.vec3) }.map {it.vec3}
+        return Spell(filter)
     }
 
     private class Spell(val blocks: List<Vec3>) : VariableMediaAction.Result(((ln(blocks.size.toDouble()) * 3) + 1).toLong() * blocks.size * MediaConstants.CRYSTAL_UNIT ) {
         override fun execute(env: CastingEnvironment): List<Iota> {
             val dense = DenseBlockPosSet()
             val center = blocks.fold(Vec3.ZERO) { acc, it ->
-                dense.add(it.blockPos.toJOML())
+                dense.add(it.blockPos.toJOML().add(0,-1,0))
                 acc.add(it)
-            }.blockPos
+            }.div(blocks.size.toDouble()).blockPos
 
             val ship = createNewShipWithBlocks(center, dense, env.world)
+            println(ship.id)
             return listOf(ShipIota(ship.id,ship.slug))
         }
 
