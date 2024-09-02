f5ae6a47e54f5bdb4ef8c375b12321c170c85c82
walksanatora <walksanatora@users.noreply.github.com>
2024-09-02 19:16:43 +0000

    Deploying to gh-pages from @ walksanatora/hexsky@b23b100aad38d1688c238ed36372228dcfb82fb2 🚀

--- docs/v/latest/master/zh_cn/index.js
+++ docs/v/latest/master/zh_cn/index.js
@@ -4,7 +4,7 @@ import semver from 'https://cdn.jsdelivr.net/npm/semver@7.5.4/+esm';
 const RELATIVE_SITE_URL = "../../../..";
 const VERSION = "latest/master";
 const MINECRAFT_VERSION = "1.20.1";
-const FULL_VERSION = "1.0.3.1.0.dev0";
+const FULL_VERSION = "1.0.4.1.0.dev0";
 const LANG = "zh_cn";
 const SHOW_DROPDOWN_MINECRAFT_VERSION = `true` === "true";
 const DROPDOWN_MINECRAFT_TEMPLATE = "Minecraft {version}";
